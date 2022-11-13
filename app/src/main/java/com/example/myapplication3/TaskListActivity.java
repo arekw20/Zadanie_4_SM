daab9d9f963162704bde7f9cf15767e788e561a9
arekw20 <arekw90@gmail.com>
2022-11-13 16:16:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication3/TaskListActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import java.util.UUID;
+
+public class TaskListActivity extends SingleFragmentActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    protected Fragment createFragment(){
+        return new TaskListFragment();
+    }
+}
